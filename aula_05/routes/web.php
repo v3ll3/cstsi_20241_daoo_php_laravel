0db2b8affe89046249474eb93b6b3d3835a0a11a
Gill D3567 <gillvelleda@gmail.com>
2024-07-05 20:57:18 -0300

    feat: criacao das rotas de teste "Olá Mundo!"

--- aula_05/routes/web.php
+++ aula_05/routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/ola',function(){
+    return view('olamundo');
+});
+
+
+Route::get('/ola/{nome}',function($nome){
+    return view('olamundo_name',["fuleco"=>$nome]);
+});
