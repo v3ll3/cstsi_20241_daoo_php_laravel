1aa5d5e5db2f2efecad012943295c3055e37cd88
Ryzen from R900 <gillvelleda@gmail.com>
2024-07-05 21:39:48 -0300

    refactor: alteração da rota ola para utilizar a classe HomeController A rota Get recebe como segundo parâmetro um array, onde o primeiro elemento é a classe e o segundo o método a ser chamado naquela rota.

--- aula_05/routes/web.php
+++ aula_05/routes/web.php
@@ -1,16 +1,14 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/ola',function(){
-    return view('olamundo');
-});
-
-
 Route::get('/ola/{nome}',function($nome){
     return view('olamundo_name',["fuleco"=>$nome]);
 });
+
+Route::get('ola',[HomeController::class,'index']);
