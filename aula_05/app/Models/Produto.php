86f1581151b6ef212cf77f8f85f95645894362bf
Gill D3567 <gillvelleda@gmail.com>
2024-07-12 16:33:17 -0300

    feat: configuradas as colunas para insercao no atributo $fillable.

--- aula_05/app/Models/Produto.php
+++ aula_05/app/Models/Produto.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Produto extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        "nome",
+        "descricao",
+        "qtd_estoque",
+        "preco",
+        "importado"
+    ];
 }
