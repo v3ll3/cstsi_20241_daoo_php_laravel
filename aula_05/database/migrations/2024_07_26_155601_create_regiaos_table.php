c176657ec679ddbf2929a03112832a38ca06254b
Gill D3567 <gillvelleda@gmail.com>
2024-07-26 14:03:40 -0300

    fix: correcao do nome da tabela

--- aula_05/database/migrations/2024_07_26_155601_create_regiaos_table.php
+++ aula_05/database/migrations/2024_07_26_155601_create_regiaos_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('regiaos', function (Blueprint $table) {
+        Schema::create('regioes', function (Blueprint $table) {
             $table->id();
             $table->string('nome', 50);
             $table->timestamps();
