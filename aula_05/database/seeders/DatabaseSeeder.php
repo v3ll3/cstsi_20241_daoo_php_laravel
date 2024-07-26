5fa4a839fc7491f70e9611215f208b59d54b1e8e
Gill D3567 <gillvelleda@gmail.com>
2024-07-26 17:00:29 -0300

    refactor: adicionada a chamada ao factory de Fornecedor com o factory de Produtos. Adicionada a chamada ao factory de Fornecedor, criando cinco fornecedores com 10 produtos  cada.

--- aula_05/database/seeders/DatabaseSeeder.php
+++ aula_05/database/seeders/DatabaseSeeder.php
@@ -13,11 +13,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create();
 
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        $this->call([
+            RegiaoSeeder::class,
+            EstadoSeeder::class
+        ]);
+
+        \App\Models\Fornecedor::factory(5)
+                ->hasProdutos(10)
+                ->create();
     }
 }
