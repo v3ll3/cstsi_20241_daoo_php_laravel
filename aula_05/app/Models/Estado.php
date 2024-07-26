8a46ba8405d545a660a5b65d175e8ce53b3df576
Gill D3567 <gillvelleda@gmail.com>
2024-07-26 16:14:10 -0300

    feat: criacada a model Estado com migration e seeder.

---
+++ aula_05/app/Models/Estado.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Estado extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nome',
+        'uf',
+        'codigouf',
+        'regiao_id'];
+
+    public function regiao(){
+        return $this->belongsTo(Regiao::class);
+    }
+}
