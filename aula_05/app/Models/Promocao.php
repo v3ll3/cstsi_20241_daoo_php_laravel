c290edc9116fa678f5b75f8a19efee00221bb741
Gill D3567 <gillvelleda@gmail.com>
2024-08-02 18:53:15 -0300

    feat: criacao da model Promocao com migration e seeder. Criacao dos tres arquivos relativos a Promocao, Model (make:model), Migration (m) e Seeder (s). php artisan make:model Promocao -ms

---
+++ aula_05/app/Models/Promocao.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Promocao extends Model
+{
+    use HasFactory;
+
+    protected $table = "promocoes";
+    protected $fillable = [
+        'nome',
+        'inicio',
+        'fim',
+        'desconto'
+    ];
+
+    public function produtos()
+    {
+        return $this->belongsToMany(Produto::class);
+    }
+}
