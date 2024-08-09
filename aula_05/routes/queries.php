8a85b3b8a9b2d8f97ed20d3f6e20040350ba56ee
Gill D3567 <gillvelleda@gmail.com>
2024-08-09 16:28:50 -0300

    feat: adicionando metodos da debugbar

--- aula_05/routes/queries.php
+++ aula_05/routes/queries.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Models\{Regiao, Estado, Fornecedor, Produto, Promocao, User};
-
+use Barryvdh\Debugbar\Facades\Debugbar;
 
 Route::prefix('queries')->group(function () {
 
@@ -27,10 +27,18 @@ Route::prefix('queries')->group(function () {
 
     Route::get('fornecedor/{id}/produtos/',function($id){
 
+        Debugbar::startMeasure('sql','Time to run query!!!');
         $result = Fornecedor::find($id)->load('produtos');
+        Debugbar::stopMeasure('sql');
 
         dump($result->toArray());
+
+        Debugbar::info($result->produtos->count());
+        Debugbar::error($result->produtos->avg('preco'));
+        Debugbar::warning($result->produtos->max('preco'));
     });
+
+
 
 });
 
