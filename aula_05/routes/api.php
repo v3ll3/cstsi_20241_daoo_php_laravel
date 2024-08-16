da2670a8e2b8f24b97beb89caa4cc4fa08371a08
Gill D3567 <gillvelleda@gmail.com>
2024-08-16 14:20:11 -0300

    feat: criacao das rotas da api para o novo recurso

--- aula_05/routes/api.php
+++ aula_05/routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\ProdutoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('produtos',ProdutoController::class);
