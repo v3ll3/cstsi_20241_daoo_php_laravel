809c057d81e737bf1d91f19a4e074e89e89ae3fa
Gill D3567 <gillvelleda@gmail.com>
2024-08-16 13:56:56 -0300

    feat: criacao do controler da api de produto. php artisan make:controller Api/ProdutoController -r --api -m Produto

---
+++ aula_05/app/Http/Controllers/Api/ProdutoController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Produto;
+use Illuminate\Http\Request;
+
+class ProdutoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Produto $produto)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Produto $produto)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Produto $produto)
+    {
+        //
+    }
+}
