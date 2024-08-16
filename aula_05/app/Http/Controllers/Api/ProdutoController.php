7bed44acc42464e902329fb060d0cbf4ed5e24e5
Gill D3567 <gillvelleda@gmail.com>
2024-08-16 14:20:34 -0300

    feat: criacao dos metodos index e show com route model binding

--- aula_05/app/Http/Controllers/Api/ProdutoController.php
+++ aula_05/app/Http/Controllers/Api/ProdutoController.php
@@ -13,7 +13,7 @@ class ProdutoController extends Controller
      */
     public function index()
     {
-        //
+        return response()->json(Produto::all());
     }
 
     /**
@@ -29,7 +29,7 @@ class ProdutoController extends Controller
      */
     public function show(Produto $produto)
     {
-        //
+        return response()->json($produto);
     }
 
     /**
